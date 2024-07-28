288ef9858639ab3a111b33b119b5c1da347ee29e
Lucia Soria <luciacsoria5@gmail.com>
2024-07-28 18:39:01 -0300

    Updating urls

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -23,7 +23,7 @@ export const HeroPage = () => {
     <div className="row mt-5">
       <div className="col-4">
         <img 
-          src={ `/heroes/${ id }.jpg`}
+          src={ `https://luciasoria5.github.io/HeroesSpa/heroes/${ id }.jpg` }
           alt={ hero.superhero }
           className="img-thumbnail animate__animated animate__fadeInLeft"
         />
